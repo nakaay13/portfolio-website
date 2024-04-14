bf35193c83cd876ae55748aef86a1d2279ee863f
nakaay13 <karbrz01@easv365.dk>
2024-04-14 20:31:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+
+<template>
+  <body>
+    <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/" exact>Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </nav>
+    </div>
+  </header>
+  </body>
+  
+
+  <RouterView />
+</template>
+
+<style scoped>
+body {
+  width: auto;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #EBE9E6;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    homee
+  </main>
+</template>
