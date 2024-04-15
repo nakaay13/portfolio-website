bf690d76456b49c341dca65c135d6c5df327e6ff
nakaay13 <karbrz01@easv365.dk>
2024-04-15 11:08:34 +0200

    update menu

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,14 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <body>
     <header>
+    <div class="top-part">
+      <div class="logo col-2"></div>
+      <div class="title col-8"></div>
+      <div class="name col-2"></div>
+    </div>
     <div class="wrapper">
       <nav>
 
-        test for git
-        saefklm
         <RouterLink to="/" exact>Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/projects">Projects</RouterLink>
@@ -19,17 +22,44 @@ import { RouterLink, RouterView } from 'vue-router'
     </div>
   </header>
   </body>
-  
-
   <RouterView />
+
 </template>
 
-<style scoped>
+<style >
 body {
   width: auto;
   height: 100%;
   margin: 0;
   padding: 0;
   background-color: #EBE9E6;
+  
+}
+header{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+.top-part {
+  width: 80%;
+  height: 150px;
+  border: 3px solid #707070;
+  border-bottom: 0px;
+  display: flex;
+  justify-content: center;
+}
+.wrapper{
+  width: 80%;
+  height: 100px;
+  border: 3px solid #707070;
+  display: flex;
+  justify-content: center;
+}
+.logo{
+  border-right: 3px solid #707070;
+}
+.title{
+  border-right: 3px solid #707070;
 }
 </style>
