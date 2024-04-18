cb477ec1e7c1860b5153e9a64a2e7f5a64466817
nakaay13 <karbrz01@easv365.dk>
2024-04-18 13:40:44 +0200

    projects almost finished

--- src/modules/getPortfolio.js
+++ src/modules/getPortfolio.js
@@ -14,22 +14,33 @@ const getPortfolio = () => {
     },
     {
       id: 2,
-      title: 'Portfolio Item 2',
-      description: 'This is the second portfolio item',
-      image: 'https://via.placeholder.com/150',
-      link: 'https://www.google.com',
-      category: 'photo'
+      title: 'TRAPHOLT POSTER 1',
+      description: 'This is the first poster I did for the Trapholt museum project. I wanted to create a poster that was simple and easy to read, while still being visually appealing. I used a combination of blobs and clean lines to create a modern and eye-catching design.',
+      image: '/src/assets/img/poster1.png',
+      link_download: '/src/assets/img/poster1.png',
+      category: 'graphic'
     },
     {
       id: 3,
-      title: 'Portfolio Item 3',
-      description: 'This is the third portfolio item',
-      category: 'web'
+      title: 'TRAPHOLT POSTER 2',
+      description: 'This is the second poster I did for the Trapholt museum project. I wanted to create a poster that was fun and engaging, while still being informative. I used a combination of bright colors and bold typography to create a design that would catch the eye of passersby.',
+      image: '/src/assets/img/poster2.png',
+      link_download: '/src/assets/img/poster2.png',
+      category: 'graphic'
     },
     {
       id: 4,
-      title: 'Portfolio Item 2',
-      description: 'This is the second portfolio item'
+      title: 'INFOGRAPHIC ABOUT GEN-Z',
+      description: 'This is an infographic I created about Gen-Z. I wanted to create a visually engaging and informative piece that would help people understand more about this generation especially how is Gen-Z changing the workplace.',
+      image: '/src/assets/img/genz.png',
+      link_download: '/src/assets/PDF/infographic.pdf',
+      category: 'graphic'
+    },
+    {
+      id: 5,
+      title: '  project 5',
+      description: 'This is a project description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien.',
+      category: 'web'
     }  
   ])
 
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -6,35 +6,64 @@
             </div>
             <div class="wrapper">
                 <ul>
-                <li class="col-3" @click="selectedCategory = 'web' ">WEB</li>
-                <li class="col-3" @click="selectedCategory = 'photo' ">GRAPHICS</li>
-                <li class="col-3" @click="selectedCategory = 'photo' ">OTHER</li>
-                <li class="col-3" @click="selectedCategory = '' ">ALL</li>
-            </ul>
+                    <li class="col-3" @click="selectedCategory = 'web' ">WEB</li>
+                    <li class="col-3" @click="selectedCategory = 'graphic' ">GRAPHICS</li>
+                    <li class="col-3" @click="selectedCategory = 'other' ">OTHER</li>
+                    <li class="col-3" @click="selectedCategory = '' ">ALL</li>
+                </ul>
             </div>
             
       
             <div class="portfolio-list">
                 <div v-for="portfolioItem in filteredPortfolioItems" :key="portfolioItem" class="card">
-                    <router-link :to="`/portfoliodetail/${portfolioItem.id}`">
-                        
-                    </router-link>
-                        <div class="photo-wrapper">
-                            <img class="project-photo" :src="portfolioItem.image" alt="">
-                        </div>
-                        <div class="text-wrapper">
-                            <p class="project-title">{{ portfolioItem.title }}</p>
-                            <p class="poject-description">{{ portfolioItem.description }}</p>
-            
-                        
+                    <router-link :to="`/portfoliodetail/${portfolioItem.id}`"></router-link>
+                    <div class="photo-wrapper">
+                        <img class="project-photo" :src="portfolioItem.image" alt="" >
+                    </div>
+                    <div class="text-wrapper">
+                        <p class="project-title">{{ portfolioItem.title }}</p>
+                        <p class="poject-description">{{ portfolioItem.description }}</p>
+        
+                    
                         <div v-if="portfolioItem.link">
-                        <a :href="portfolioItem.link">See more</a>
+                            <a :href="portfolioItem.link" target="_blank">See more</a>
+                        </div>
+                        <div v-if="portfolioItem.link_download">
+                            <a :href="portfolioItem.link_download" download>Download here</a>
+                        </div>
+                        <div v-else></div>
+                    </div>
+                </div>
+            </div>
+            <div class="portfolio-list-media">
+                <div v-for="portfolioItem in filteredPortfolioItems" :key="portfolioItem" class="card-media">
+                    <router-link :to="`/portfoliodetail/${portfolioItem.id}`"></router-link>
+                    <div class="text-wrapper-media">
+                        <div class="photo-wrapper-media">
+                            <img class="project-photo-media" :src="portfolioItem.image" alt="" >
+                        </div>
+                        <p class="project-title-media">{{ portfolioItem.title }}</p>
+                        
+                    </div>
+                    
+                    
+                        <div class="bottom-part">
+                            <p class="project-title-media2">{{ portfolioItem.title }}</p>
+                            <p class="poject-description-media">{{ portfolioItem.description }}</p>
+        
+                    
+                        <div v-if="portfolioItem.link">
+                            <a class="more" :href="portfolioItem.link" target="_blank">See more</a>
+                        </div>
+                        <div v-if="portfolioItem.link_download">
+                            <a class="more" :href="portfolioItem.link_download" download>Download here</a>
                         </div>
                         <div v-else></div>
                         </div>
+
                         
                     
-            </div>
+                </div>
             </div>
       
         </div>  
@@ -42,6 +71,13 @@
   </template>
   
   <style scoped>
+    .project-title-media2 {
+        display: none;
+    }
+    .portfolio-list-media {
+        display: none;
+        
+    }
     .portfolio-part {
         display: flex;
         justify-content: center;
@@ -52,7 +88,7 @@
     .wrapper{
         width: 80%;
         height: 100px;
-        border: 3px solid var(--secondary-grey);
+        border: var(--border-width) solid var(--secondary-grey);
         display: flex;
         justify-content: center;
         }
@@ -76,7 +112,7 @@
         list-style-type: none;
     }
     li{
-        border-right: 3px solid var(--secondary-grey);
+        border-right: var(--border-width) solid var(--secondary-grey);
         height: 100%;
         display: flex;
         justify-content: center;
@@ -95,15 +131,15 @@
         display: flex;
         justify-content: center;
         flex-direction: column;
-        border: 3px solid var(--secondary-grey);
+        border: var(--border-width) solid var(--secondary-grey);
         border-top: 0px;
         color: var(--secondary-grey);
         padding: 20px;
         padding-bottom: 0px;
     }
     .card {
-        height: 300px;
-        border: 3px solid var(--secondary-grey);
+        height: 400px;
+        border: var(--border-width) solid var(--secondary-grey);
         display: flex;
         flex-direction: row;
         margin-bottom: 20px;
@@ -128,11 +164,12 @@
         margin-bottom: 10px;
     }
     .photo-wrapper {
-        border-right: 3px solid var(--secondary-grey);
-        width: 100%;
+        
+        width: 500px;
     }
     .project-photo {
-        width: 100%;
+        border-right: var(--border-width) solid var(--secondary-grey);
+        width: auto;
         height: 100%;
         object-fit: cover;
         object-position: center top;
@@ -141,6 +178,166 @@
         font-size: 20px;
         font-family: 'Tinos', serif;
         color: var(--secondary-grey);
+    }
+    @media (max-width:1200px){
+        .card {
+            height: 500px;
+        }
+    }
+    @media (max-width:1000px){
+        .header {
+            font-size: 70px;
+            height: 120px;
+
+        }
+        .wrapper{
+            height: 70px;
+        }
+        li{
+            font-size: 25px;
+        }       
+        .portfolio-list{
+            display: none;
+        }
+        .portfolio-list-media {
+            width: 80%;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            border: var(--border-width) solid var(--secondary-grey);
+            border-top: 0px;
+            color: var(--secondary-grey);
+            padding: 20px;
+            padding-bottom: 0px;
+        }
+        .card-media {
+            height: auto;
+            border: var(--border-width) solid var(--secondary-grey);
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 20px;
+        }
+    
+        .text-wrapper-media {
+            display: flex;
+            flex-direction: row;
+            padding: 20px;
+            color: var(--secondary-grey);
+            height: 300px;
+            padding-bottom: 0px;
+        }
+        .project-title-media {
+            font-size: 50px;
+            font-family: 'Abril Fatface';
+            margin-top: 150px;
+        }
+        .poject-description-media {
+            font-size: 20px;
+            font-family: 'Tinos', serif;
+            margin: 20px;
+            margin-bottom: 10px;
+            margin-top: 10px;
+        }
+        .photo-wrapper-media {
+            width: auto;
+            border: 3px solid var(--secondary-grey);
+            
+        }
+        .project-photo-media {
+            
+            width: auto;
+            height: 100%;
+            object-fit: cover;
+            object-position: center top;
+        }
+        .more {
+            font-size: 20px;
+            font-family: 'Tinos', serif;
+            color: var(--secondary-grey);
+            margin: 20px;
+        }
+    }
+    @media (max-width: 800px){
+        .header {
+            font-size: 60px;
+            height: 100px;
+        }
+        .wrapper{
+            height: 50px;
+        }
+        li{
+            font-size: 20px;
+        }
+        .project-title-media{
+            font-size: 40px;
+            margin-top: 180px;
+        }
+        .poject-description-media {
+            font-size: 15px;
+        }
+        .more {
+            font-size: 15px;
+        }
+
+    }
+    @media (max-width: 620px){
+        .project-title-media{
+            display: none;
+        }
+        .project-title-media2 {
+            display: flex;
+            font-size: 40px;
+            font-family: 'Abril Fatface';
+            margin: 10px 20px 10px 20px;
+        }
+        .header {
+            font-size: 40px;
+            height: 80px;
+        }
+        .wrapper{
+            height: 40px;
+            border: var(--border-small) solid var(--secondary-grey);
+        }
+        li{
+            font-size: 15px;
+            border-right: var(--border-small) solid var(--secondary-grey);
+        }
+        .photo-wrapper-media {
+            width: 100%;
+            border: var(--border-small) solid var(--secondary-grey); 
+            height: auto; 
+            max-height: 1000px;
+            overflow: hidden; 
+        }
+        .project-photo-media {
+            width: 100%;
+            height: 100%; 
+            object-fit: cover; 
+        }
+        .portfolio-list-media {
+            border: var(--border-small) solid var(--secondary-grey);
+            border-top: 0px;
+            border-bottom: 0px;
+        }
+        .card-media {
+            border: var(--border-small) solid var(--secondary-grey);
+        }
+
+    }
+    @media(max-width:500px){
+        .header {
+             font-size: 30px;
+             height: 60px;
+        }
+        .wrapper{
+            height: 30px;
+        }
+        li{
+            font-size: 10px;
+        }
+        .project-title-media2 {
+            font-size: 25px;
+        }
     }
   </style>
 
